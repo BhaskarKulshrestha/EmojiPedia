39a9eb9b0884f771084e0b147b1998f6a1fe154a
Bhaskar Kulshrestha <bhaskarkulshrestha03@gmail.com>
2023-09-23 18:32:12 +0530

    buld folder added

---
+++ build/precache-manifest.208f5816ac22b6ff89905bb874dfce2d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5db1747bb1fbcf84a3a5a05ff0874de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee9e355d3438769b7034",
+    "url": "/static/js/2.88831c34.chunk.js"
+  },
+  {
+    "revision": "1794a9a13f81d524f8f0",
+    "url": "/static/js/main.d01fb9be.chunk.js"
+  },
+  {
+    "revision": "e3c7c7483778e1bff734",
+    "url": "/static/js/runtime-main.9435702c.js"
+  }
+]);
